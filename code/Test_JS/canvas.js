68b87a8fbefcf330a449808ed2d9ce5827e9104a
Themishau <40619652+Themishau@users.noreply.github.com>
2020-10-20 14:40:51 +0200

    test

--- code/Test_JS/canvas.js
+++ code/Test_JS/canvas.js
@@ -1,14 +1,101 @@
+/* Canvas */
+const canvas = document.getElementById("ArkanoidCanvas");
+const canvasContext = canvas.getContext("2d");
+let kspace = false;
+let kleftA = false;
+let krightA = false;
 
-
-// information about canvas
-function draw()
+/* keyboard listener */
+document.addEventListener("keydown", function(event)
 {
+    /* as there might be some support issues we have to check the property of the key pressed */
+    if(event.which || event.charCode || event.keyCode )
+    {
+        var characterCode = event.which || event.charCode || event.keyCode;
+    }
+    else if (event.key!=undefined){
+        var characterCode = charCodeArr[event.key] || event.key.charCodeAt(0);
+    }
+    else
+    {
+        var characterCode = 0;
+    }
+    /* check saved key */
+    if(characterCode == 37)
+    {
+        kleftA = true;
+    }
+    else if(characterCode == 39)
+    {
+        krightA = true;
+    }    
+    if(characterCode == 32)
+    {
+        kspace = true;
+    } 
+});
+document.addEventListener("keyup", function(event)
+{
+    /* as there might be some support issues we have to check the property of the key pressed */
+    if(event.which || event.charCode || event.keyCode )
+    {
+        var characterCode = event.which || event.charCode || event.keyCode;
+    }
+    else if (event.key!=undefined)
+    {
+        var characterCode = charCodeArr[event.key] || event.key.charCodeAt(0);
+    }
+    else
+    {
+        var characterCode = 0;
+    }
+    /* check saved key */
+    if(characterCode == 37)
+    {
+        kleftA = false;
+    }
+    else if(characterCode == 39)
+    {
+        krightA = false;
+    }
+    if(characterCode == 32)
+    {
+        kspace = true;
+    } 
+});
 
+/* Paddel_Info */
+const paddel_width = 120;
+const paddel_height = 20;
+const paddel_margin_bottom = 20; 
+const paddel =  {
+                    x      : canvas.width/2 - paddel_width/2,
+                    y      : canvas.height - paddel_height/0.3,
+                    width  : paddel_width,
+                    height : paddel_height,
+                    dx     : 10 // speed of movement right and left 
+                };
+/* background */
+const bgImg = new Image();
+
+bgImg.src = "FHDW_Logo.jpg";
+
+
+// draws paddel
+function drawpaddel()
+{   
+    canvasContext.fillStyle = "grey";
+    canvasContext.fillRect(paddel.x, paddel.y, paddel.width, paddel.height);   
+    canvasContext.strokeStyle = "green";
+    canvasContext.strokeRect(paddel.x, paddel.y, paddel.width, paddel.height);
 }
+
 function updatescreen()
 {
-
+    clearCanvasRect();
+    drawpaddel();
 }
+
 function drawRect(x, y)
 {
     canvasContext.fillStyle = "green"; // red
@@ -23,45 +110,49 @@ function clearCanvasRect()
 
 function drawbackground(img)
 {
-
     img.onload = function () 
                     {
                      canvasContext.drawImage(img, 0, 0, canvas.width, canvas.height);
                     };
 }
-function drawLoop()
+function paddelmovement()
 {
-
-
-//drawLoop();
-// playing around with canvas
-drawRect(100, 200);
-drawRect(100, 250);
-drawRect(100, 300);
-drawbackground(g);
-draw();
-updatescreen();
-//requestAnimationFrame(drawLoop);
+    if(kleftA && (paddel.x > 0))
+    {
+        paddel.x -= paddel.dx;
+    }
+    else if(krightA && (paddel.x + paddel_width < canvas.width))
+    {
+        paddel.x += paddel.dx;
+    }
 
 }
 
+function drawLoop()
+{
 
-// get Canvas
-
-const canvas = document.getElementById("ArkanoidCanvas");
+    /*
+    playing around with canvas
+    drawRect(100, 200);
+    drawRect(100, 250);
+    drawRect(100, 300);
+    */
+   // drawbackground(bgImg);
+    drawpaddel();
+    paddelmovement();
+    updatescreen();
+    
+    requestAnimationFrame(drawLoop);
+}
 
 // choose method properties
-const canvasContext = canvas.getContext("2d");
-
-
 
 // seltsamerweise muss eine Variable vom Typ Image img im Variablennamen haben 
 /*
 var BILD = new Image();
 BILD.scr = 'FHDW_Logo.jpg';
 */
-var g = new Image();
-g.src = "FHDW_Logo.jpg";
+
 
 
 drawLoop();
--- code/Test_JS/canvas2.js
+++ code/Test_JS/canvas2.js
@@ -4,14 +4,14 @@
 var image = new Image();
 image.scr = 'CjOn_MSWkAEqsIP.jpg';
 
-var i = new Image();
-i.src = "index.jpg";
+var imga = new Image();
+imga.src = "index.jpg";
 
 const canvas2 = document.getElementById("ArkanoidCanvas2");
 const canvasContext2 = canvas2.getContext("2d");
-i.onload = function () 
+imga.onload = function () 
 {
-    canvasContext2.drawImage(i, 0, 0, canvas2.width, canvas2.height);
+    canvasContext2.drawImage(imga, 0, 0, canvas2.width, canvas2.height);
 };
 
 
