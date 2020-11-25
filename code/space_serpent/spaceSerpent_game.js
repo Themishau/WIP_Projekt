fe41325a478c48d5e935c0e7fc5ae82d49864ff6
Themishau <40619652+Themishau@users.noreply.github.com>
2020-11-25 14:53:01 +0100

    updated sources and sound effects
    Co-Authored-By: KaBrat <48164704+KaBrat@users.noreply.github.com>

--- code/space_serpent/spaceSerpent_game.js
+++ code/space_serpent/spaceSerpent_game.js
@@ -89,6 +89,7 @@ var soundEffects = [
     "explosion.wav",
     "foodPlayer.wav",
     "lost.wav",
+    "loseSound.wav",
     "backpack.wav",
     "wibl001.wav",
     "Debris1.wav",
@@ -506,6 +507,8 @@ class LevelConfig {
         this.sound[6].volume = 0.1;
         this.sound[7] = assets.lost;
         this.sound[7].volume = 0.1;
+        this.sound[8] = assets.loseSound;
+        this.sound[8].volume = 0.1;
         console.log("sounds", this.sound);
         this.sound.volume = 0.1;
         //this.obstacleTable = new playground(1, null, null);
@@ -690,7 +693,7 @@ class level {
             //else if (this.levelConfig.highestEnemy == this.levelConfig.levelOption.winCodition || this.levelConfig.serpentPlayer.isAlive == false) {    
             this.levelConfig.playGroundLevel.bgsound.pause();
             this.levelConfig.playGroundLevel.bgsound.currentTime = 0;
-            this.levelConfig.sound[7].play();
+            //this.levelConfig.sound[8].play();
             //victorySoundeffect(this.levelConfig.sound);
             let gameMode = getGameInstance();
             //console.log("this state is: ", gameMode);
@@ -2133,6 +2136,7 @@ function movePlayer(serpent, playField, items, sound) {
 
         }
         killSerpent(serpent, playField, items);
+        sound[8].play();
     }
 }
 
