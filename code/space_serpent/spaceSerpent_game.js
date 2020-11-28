97cb2db70d948573e720108698ea97aff4a34b4d
Themishau <40619652+Themishau@users.noreply.github.com>
2020-11-28 16:37:27 +0100

    Revert "Update spaceSerpent_game.js"
    This reverts commit 024391cdb0a1cefeaf181e20cf25b6bd1f821d52.

--- code/space_serpent/spaceSerpent_game.js
+++ code/space_serpent/spaceSerpent_game.js
@@ -1599,9 +1599,6 @@ var highScoreTable = {
                 this.popScoreSheetButtons();
             }
             document.body.removeChild(this.highScoreCanvas);
-<<<<<<< HEAD
->>>>>>> parent of e109b48... ScreenTableDynamicInitial
-=======
 >>>>>>> parent of e109b48... ScreenTableDynamicInitial
         }
         document.body.removeChild(this.highScoreCanvas);
@@ -1763,9 +1760,6 @@ var instruction ={
 <<<<<<< HEAD
 =======
     arrows: new Image(),
-<<<<<<< HEAD
->>>>>>> parent of e109b48... ScreenTableDynamicInitial
-=======
 >>>>>>> parent of e109b48... ScreenTableDynamicInitial
 
     init: function(){
