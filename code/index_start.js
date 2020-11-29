6f6a0b80741ca0172c75a9fd9fcf284334dd3ed1
MarkSilasPiechowiak <72301711+MarkSilasPiechowiak@users.noreply.github.com>
2020-11-29 17:44:20 +0100

    currentStatus

--- code/index_start.js
+++ code/index_start.js
@@ -1,19 +1,49 @@
-window.onload = function(){
-    
-}
+var startGame = false;
 
 var gameAutomat = {
     gameAutomatImage: null,
+    gameAnimation: null,
+    startButton: null,
     currentElement: null,
 
     
     init: function(){
+        this.loadImage();
+        this.loadAnimation();
+        this.loadButton();
+    },
+
+    startGame: function(){
+        window.location.href = "space_serpent/space_serpent_main.html";
+    },
+
+    loadButton: function(){
+        this.startButton = document.createElement("img");
+        this.startButton.onclick = this.startGame();
+    },
+
+    loadAnimation:function(){
+        this.gameAnimation = document.createElement("img");
+        this.gameAnimation.height = window.innerHeight/5;
+        this.gameAnimation.width = window.innerHeight/5;
+        this.currentElement = document.getElementById("gameDiv");
+        this.currentElement.insertBefore(this.gameAnimation, this.currentElement.childNodes[0]);
+    },
+
+    loadImage: function(){
         this.gameAutomatImage = document.createElement("img");
         this.gameAutomatImage.src = "img_folder/gamingautomat.png";
         this.gameAutomatImage.height = window.innerHeight;
         this.currentElement = document.getElementById("div_gamingAutomat");
-        console.log(this.currentElement, this.gameAutomatImage);
-        this.currentElement.insertBefore(this.gameAutomatImage, this.currentElement.childNodes[1]);
+        this.currentElement.insertBefore(this.gameAutomatImage, this.currentElement.childNodes[0]);
+    }
+}
+
+window.onload = function(){
+    gameAutomat.init();
+    if(startGame == false){
+
+    }else{
 
     }
 }
