9c349e66ebc5a106af4b4cf67b0c1f1dfb9aa2b2
piechowiakmp <46462963+piechowiakmp@users.noreply.github.com>
2020-12-02 13:32:45 +0100

    currentStatePreview

--- code/index_start.js
+++ code/index_start.js
@@ -20,7 +20,7 @@ var gameAutomat = {
     loadCanvas: function (){
         this.gameAutomatCanvas = document.createElement("canvas");
         this.gameAutomatCanvas.id = "Gameautomat";
-        this.gameAutomatCanvas.width = 1000;
+        this.gameAutomatCanvas.width = 3500;
         this.gameAutomatCanvas.height = window.innerHeight;
         this.gameAutomatCanvas.style.border = "2px solid black";
         this.gameAutomatCanvasContext = this.gameAutomatCanvas.getContext("2d");
@@ -34,7 +34,7 @@ var gameAutomat = {
         this.loadImage();
         this.loadAnimation();
         this.startButton.src= "img_folder/buttonPressed.png",
-        this.gameAutomatCanvasContext.drawImage(this.startButton,320 ,this.gameAutomatCanvas.height/1.19, this.gameAutomatCanvas.height/6, this.gameAutomatCanvas.height/6);
+        this.gameAutomatCanvasContext.drawImage(this.startButton,this.gameAutomatCanvas.height/2.7 ,this.gameAutomatCanvas.height/1.19, this.gameAutomatCanvas.height/6, this.gameAutomatCanvas.height/6);
         sleep(2000);
         window.location.href = "space_serpent/space_serpent_main.html";
     },
@@ -42,13 +42,13 @@ var gameAutomat = {
     loadButton: function(){
         this.startButton = new Image();
         this.startButton.src = "img_folder/buttonUnpressed.png";
-        this.gameAutomatCanvasContext.drawImage(this.startButton,320 ,this.gameAutomatCanvas.height/1.19, this.gameAutomatCanvas.height/6, this.gameAutomatCanvas.height/6);
+        this.gameAutomatCanvasContext.drawImage(this.startButton, this.gameAutomatCanvas.height/2.7 ,this.gameAutomatCanvas.height/1.19, this.gameAutomatCanvas.height/6, this.gameAutomatCanvas.height/6);
     },
 
     loadAnimation:function(){
         this.gameAnimation = new Image();
         this.gameAnimation.src = "img_folder/animation1.png";
-        this.gameAutomatCanvasContext.drawImage(this.gameAnimation,125 ,this.gameAutomatCanvas.height/3.3, this.gameAutomatCanvas.height/1.41, this.gameAutomatCanvas.height/1.67);
+        this.gameAutomatCanvasContext.drawImage(this.gameAnimation, this.gameAutomatCanvas.height/7 ,this.gameAutomatCanvas.height/3.3, this.gameAutomatCanvas.height/1.41, this.gameAutomatCanvas.height/1.67);
     },
 
     loadImage: function(){
@@ -94,6 +94,7 @@ function KeyDown(event){
     let characterCode = event.keyCode;
     if(characterCode == 13 || characterCode == 32){
         this.gameAutomat.startGame();
+        
     }
 }
 
